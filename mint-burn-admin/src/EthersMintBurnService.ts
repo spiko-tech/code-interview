52ef3609f8468a86460eee6fa0e3f7d60c905a81
wewelll <samuel.briole@gmail.com>
2024-06-13 11:09:18 +0200

    fix build

--- mint-burn-admin/src/EthersMintBurnService.ts
+++ mint-burn-admin/src/EthersMintBurnService.ts
@@ -1,5 +1,11 @@
+import { BigNumberish } from 'ethers';
 import { MintBurnService } from './interface/MintBurnService';
 
 export class EthersMintBurnService implements MintBurnService {
-  // code here
+  burn = (amount: BigNumberish, address: string) => {
+    throw new Error('Method not implemented.');
+  };
+  mint = (amount: BigNumberish, address: string) => {
+    throw new Error('Method not implemented.');
+  };
 }
--- off-chain-registry/src/EthersIndexingService.ts
+++ off-chain-registry/src/EthersIndexingService.ts
@@ -1,5 +1,7 @@
 import { IndexingService } from './interface/IndexingService';
 
 export class EthersIndexingService implements IndexingService {
-  // code here
+  getTransfers = () => {
+    throw new Error('Method not implemented.');
+  };
 }
